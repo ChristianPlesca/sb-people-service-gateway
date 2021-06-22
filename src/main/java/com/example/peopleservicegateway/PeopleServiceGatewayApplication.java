b977dfee29569b3126efd91c0aaf1d74524d464b
Cristian Plesca <cplesca@zaizi.com>
2021-06-22 12:44:43 +0100

    Initial Commit

---
+++ src/main/java/com/example/peopleservicegateway/PeopleServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.peopleservicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/peopleservicegateway/PeopleServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peopleservicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
